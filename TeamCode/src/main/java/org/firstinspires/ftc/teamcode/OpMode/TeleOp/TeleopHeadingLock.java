a2f83dd6aab657c1db679b054455596318f5828c
Neurobots1 <neurobots@classomption.qc.ca>
2025-10-20 16:18:40 -0400

    Test_pedro fixed

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/TeleopHeadingLock.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.OpMode.TeleOp;
+
+
+
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+public class TeleopHeadingLock extends OpMode {
+
+    public static Pose startingPose;
+    public static Follower follower;
+    private TelemetryManager telemetryM;
+
+
+
+
+    @Override
+    public void init() {
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startingPose == null ? new Pose() : startingPose);
+        follower.update();
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+        follower.startTeleopDrive();
+}
+
+@Override
+public void loop(){
+
+        double Heading= Math.atan2(2,2);
+
+    follower.setTeleOpDrive(
+            -gamepad1.left_stick_y ,
+            -gamepad1.left_stick_x,
+            Heading,
+            false // Robot Centric
+    );
+    }
+}
