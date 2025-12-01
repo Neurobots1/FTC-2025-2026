b0a8e55a76582773da2067ae91baf668408997ca
Neurobots1 <neurobots@classomption.qc.ca>
2025-12-01 16:27:29 -0500

    ShooterController Not working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/newTeleopTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOp/newTeleopTest.java
@@ -10,7 +10,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.SubSystem.Shoot;
 
-
 @Configurable
 @TeleOp
 public class newTeleopTest extends OpMode {
@@ -19,39 +18,24 @@ public class newTeleopTest extends OpMode {
     public Shoot shooter;
     public static double Target = 0;
 
-    public static double p = 0.1;
-
-    public static double i;
-
-    public static double d;
-
     private TelemetryManager telemetryM;
     private PanelsTelemetry panelsTelemetry;
 
     public void init(){
 
         telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+        shooter = new Shoot( hardwareMap);
+
     }
 
 
     public void loop(){
 
-        shooter = new Shoot( hardwareMap,
-                p, i, d
-        );
-        if (gamepad1.a){
-            Target= Target+100;
-        }
-
-        if (gamepad1.b){
-            Target = Target-100;
-        }
-
         shooter.setTargetRPM(Target);
-        shooter.update();
-        telemetryM.debug("TargetRpm", shooter.getTargetRPM());
-        telemetryM.debug("averageRpm", shooter.getAverageRPM());
-        telemetryM.addData("CurrentRpm1", shooter.getCurrentRPM1());;
+        telemetryM.addData("shooter Rpm", shooter.getAverageRPM());
+        telemetryM.addData("Target Rpm", shooter.getTargetRPM());
+        telemetry.update();
         telemetryM.update();
+        shooter.update();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/Shoot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystem/Shoot.java
@@ -1,37 +1,39 @@
 package org.firstinspires.ftc.teamcode.SubSystem;
 
+import com.bylazar.configurables.annotations.Configurable;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Configurable
 public class Shoot {
 
-    public DcMotorEx shootMotor1;
-    public DcMotorEx shootMotor2;
-
-    public Servo hoodServo;
-    public Servo OnOff;
+    public DcMotorEx ShooterA;
+    public DcMotorEx ShooterB;
 
     private PIDController controller1;
-    private PIDController controller2;
-
     private double targetRPM = 0;
 
     private double currentRPM1 = 0;
     private double currentRPM2 = 0;
 
+            public static double p= 0;
+            public static double i = 0;
+            public static double d= 0;
+            public static  double f=0;
+
     private static final double TICKS_PER_REV = 28.0; // adjust if needed
 
 
-    public Shoot(HardwareMap hardwareMap, double p, double i, double d) {
-        shootMotor1 = hardwareMap.get(DcMotorEx.class, "shootMotor1");
-        shootMotor1.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        shootMotor1.setDirection(DcMotorSimple.Direction.REVERSE);
+    public Shoot(HardwareMap hardwareMap) {
+        ShooterA = hardwareMap.get(DcMotorEx.class, "ShooterA");
+        ShooterA.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        ShooterA.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        shootMotor2 = hardwareMap.get(DcMotorEx.class, "shootMotor2");
-        shootMotor2.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        shootMotor2.setDirection(DcMotorSimple.Direction.FORWARD);
+        ShooterB = hardwareMap.get(DcMotorEx.class, "ShooterB");
+        ShooterB.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        ShooterB.setDirection(DcMotorSimple.Direction.FORWARD);
 
         controller1 = new PIDController(p, i, d);
     }
@@ -42,9 +44,11 @@ public class Shoot {
     }
 
         public void update() {
+
+            controller1 = new PIDController(p, i, d);
             // Convert ticks/s to RPM
-            currentRPM1 = shootMotor1.getVelocity() * 60.0 / TICKS_PER_REV;
-            currentRPM2 = shootMotor2.getVelocity() * 60.0 / TICKS_PER_REV;
+            currentRPM1 = ShooterA.getVelocity() * 60.0 / TICKS_PER_REV;
+            currentRPM2 = ShooterB.getVelocity() * 60.0 / TICKS_PER_REV;
 
             double currentRPM = (currentRPM1 + currentRPM2) / 2.0;
 
@@ -52,8 +56,8 @@ public class Shoot {
 
             power = Math.max(0, Math.min(1, power));
 
-            shootMotor1.setPower(power);
-            shootMotor2.setPower(power);
+            ShooterA.setPower(power);
+            ShooterB.setPower(power);
         }
 
     public double getTargetRPM() {
